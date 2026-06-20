80b32606fed3eea19dfc2357d020a69307fc8e73
Neeraj899700 <Neeraj899700@users.noreply.github.com>
2026-06-20 13:10:42 +0000

    Revert to working build: extends Screen mixin

--- src/main/java/dev/afkz/AfkzClientCosmeticCore.java
+++ src/main/java/dev/afkz/AfkzClientCosmeticCore.java
@@ -1,12 +1,6 @@
 package dev.afkz;
 
-import dev.afkz.screen.CosmeticScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,24 +11,6 @@ public class AfkzClientCosmeticCore implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         LOGGER.info("[Afkz Client Cosmetic Core] Initializing...");
-
-        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-            String screenTitle = screen.getTitle().getString();
-            if (screenTitle.isEmpty() || screen.getClass().getSimpleName().equals("TitleScreen")) {
-                Button cosmeticsButton = Button.builder(
-                    Component.literal("Cosmetics"),
-                    button -> client.setScreen(new CosmeticScreen())
-                ).bounds(
-                    screen.width / 2 - 100,
-                    screen.height / 2 + 24,
-                    200,
-                    20
-                ).build();
-
-                screen.addRenderableWidget(cosmeticsButton);
-            }
-        });
-
         LOGGER.info("[Afkz Client Cosmetic Core] Loaded successfully!");
     }
 }
