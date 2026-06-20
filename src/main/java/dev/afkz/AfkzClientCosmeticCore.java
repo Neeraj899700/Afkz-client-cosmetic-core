44c40da5be771222661711b08e0bad14872ff485
Neeraj899700 <Neeraj899700@users.noreply.github.com>
2026-06-20 12:18:49 +0000

    Initial Fabric mod setup for MC 26.1.2

---
+++ src/main/java/dev/afkz/AfkzClientCosmeticCore.java
@@ -0,0 +1,16 @@
+package dev.afkz;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AfkzClientCosmeticCore implements ClientModInitializer {
+    public static final String MOD_ID = "afkz-client-cosmetic-core";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("[Afkz Client Cosmetic Core] Initializing...");
+        LOGGER.info("[Afkz Client Cosmetic Core] Loaded successfully!");
+    }
+}
