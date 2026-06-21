20cfa85bc14aa7db40f536dba29d0c39f23e5481
Neeraj899700 <Neeraj899700@users.noreply.github.com>
2026-06-21 04:04:31 +0000

    Small 20x20 icon button next to accessibility

--- src/main/java/dev/afkz/mixin/TitleScreenMixin.java
+++ src/main/java/dev/afkz/mixin/TitleScreenMixin.java
@@ -1,15 +1,12 @@
 package dev.afkz.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import dev.afkz.AfkzClientCosmeticCore;
 import dev.afkz.screen.CosmeticScreen;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,8 +16,6 @@ import java.lang.reflect.Method;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-
-    private static final ResourceLocation AFKZ_ICON = ResourceLocation.fromNamespaceAndPath("afkz", "textures/cosmetic_icon.png");
 
     private static Method addRenderableWidgetMethod;
 
@@ -46,41 +41,31 @@ public class TitleScreenMixin {
             TitleScreen self = (TitleScreen) (Object) this;
             Minecraft mc = Minecraft.getInstance();
 
-            int accessX = 4;
-            int accessY = self.height - 24;
             int btnW = 20;
             int btnH = 20;
+            int accessX = 4;
+            int accessY = self.height - 24;
             int cosmeticsX = accessX + btnW + 2;
             int cosmeticsY = accessY;
 
-            AfkzClientCosmeticCore.LOGGER.info("[Afkz] Adding cosmetics icon at ({}, {})", cosmeticsX, cosmeticsY);
+            AfkzClientCosmeticCore.LOGGER.info("[Afkz] Adding cosmetics button at ({}, {})", cosmeticsX, cosmeticsY);
 
-            Button btn = new Button(cosmeticsX, cosmeticsY, btnW, btnH,
-                Component.literal("Cosmetics"),
+            Button btn = Button.builder(
+                Component.literal("\u2728"),
                 b -> {
-                    AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics icon clicked!");
+                    AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics button clicked!");
                     mc.setScreen(new CosmeticScreen());
                 }
-            ) {
-                @Override
-                public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
-                    RenderSystem.enableBlend();
-                    RenderSystem.setShaderTexture(0, AFKZ_ICON);
-                    guiGraphics.blit(AFKZ_ICON, this.getX(), this.getY(), 0, 0, this.width, this.height, this.width, this.height);
-                    if (this.isHoveredOrFocused()) {
-                        guiGraphics.renderTooltip(guiGraphics, this.createTooltip(), mouseX, mouseY);
-                    }
-                }
-            };
+            ).bounds(cosmeticsX, cosmeticsY, btnW, btnH).build();
 
             if (addRenderableWidgetMethod != null) {
                 addRenderableWidgetMethod.invoke(self, btn);
-                AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics icon added!");
+                AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics button added!");
             } else {
-                AfkzClientCosmeticCore.LOGGER.error("[Afkz] Cannot add icon - method not found");
+                AfkzClientCosmeticCore.LOGGER.error("[Afkz] Cannot add button - method not found");
             }
         } catch (Exception e) {
-            AfkzClientCosmeticCore.LOGGER.error("[Afkz] Failed to add icon!", e);
+            AfkzClientCosmeticCore.LOGGER.error("[Afkz] Failed to add button!", e);
         }
     }
 }
