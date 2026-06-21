4286d21f634142946462e53bda2e95c26b7f3be1
Neeraj899700 <Neeraj899700@users.noreply.github.com>
2026-06-21 04:03:00 +0000

    Change to small icon button next to accessibility icon

--- src/main/java/dev/afkz/mixin/TitleScreenMixin.java
+++ src/main/java/dev/afkz/mixin/TitleScreenMixin.java
@@ -1,12 +1,15 @@
 package dev.afkz.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.afkz.AfkzClientCosmeticCore;
 import dev.afkz.screen.CosmeticScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,6 +19,8 @@ import java.lang.reflect.Method;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
+
+    private static final ResourceLocation AFKZ_ICON = ResourceLocation.fromNamespaceAndPath("afkz", "textures/cosmetic_icon.png");
 
     private static Method addRenderableWidgetMethod;
 
@@ -29,14 +34,6 @@ public class TitleScreenMixin {
                     break;
                 }
             }
-            if (addRenderableWidgetMethod == null) {
-                AfkzClientCosmeticCore.LOGGER.warn("[Afkz] addRenderableWidget not found, trying alternatives...");
-                for (Method m : Screen.class.getDeclaredMethods()) {
-                    if (m.getName().contains("Widget") || m.getName().contains("widget")) {
-                        AfkzClientCosmeticCore.LOGGER.info("[Afkz] Found method: {} -> {}", m.getName(), m.getReturnType());
-                    }
-                }
-            }
         } catch (Exception e) {
             AfkzClientCosmeticCore.LOGGER.error("[Afkz] Reflection setup failed", e);
         }
@@ -48,26 +45,42 @@ public class TitleScreenMixin {
         try {
             TitleScreen self = (TitleScreen) (Object) this;
             Minecraft mc = Minecraft.getInstance();
-            int x = self.width / 2 - 100;
-            int y = self.height / 2 + 24;
-            AfkzClientCosmeticCore.LOGGER.info("[Afkz] Adding button at ({}, {})", x, y);
 
-            Button btn = Button.builder(
+            int accessX = 4;
+            int accessY = self.height - 24;
+            int btnW = 20;
+            int btnH = 20;
+            int cosmeticsX = accessX + btnW + 2;
+            int cosmeticsY = accessY;
+
+            AfkzClientCosmeticCore.LOGGER.info("[Afkz] Adding cosmetics icon at ({}, {})", cosmeticsX, cosmeticsY);
+
+            Button btn = new Button(cosmeticsX, cosmeticsY, btnW, btnH,
                 Component.literal("Cosmetics"),
                 b -> {
-                    AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics button clicked!");
+                    AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics icon clicked!");
                     mc.setScreen(new CosmeticScreen());
                 }
-            ).bounds(x, y, 200, 20).build();
+            ) {
+                @Override
+                public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+                    RenderSystem.enableBlend();
+                    RenderSystem.setShaderTexture(0, AFKZ_ICON);
+                    guiGraphics.blit(AFKZ_ICON, this.getX(), this.getY(), 0, 0, this.width, this.height, this.width, this.height);
+                    if (this.isHoveredOrFocused()) {
+                        guiGraphics.renderTooltip(guiGraphics, this.createTooltip(), mouseX, mouseY);
+                    }
+                }
+            };
 
             if (addRenderableWidgetMethod != null) {
                 addRenderableWidgetMethod.invoke(self, btn);
-                AfkzClientCosmeticCore.LOGGER.info("[Afkz] Button added via reflection!");
+                AfkzClientCosmeticCore.LOGGER.info("[Afkz] Cosmetics icon added!");
             } else {
-                AfkzClientCosmeticCore.LOGGER.error("[Afkz] Cannot add button - method not found");
+                AfkzClientCosmeticCore.LOGGER.error("[Afkz] Cannot add icon - method not found");
             }
         } catch (Exception e) {
-            AfkzClientCosmeticCore.LOGGER.error("[Afkz] Failed to add button!", e);
+            AfkzClientCosmeticCore.LOGGER.error("[Afkz] Failed to add icon!", e);
         }
     }
 }
